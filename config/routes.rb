24545d4cbb08311df2e0f9f734085d11808a75ca
andela-odurodola <Oluwadamilola.durodola@andela.com>
2017-09-28 17:08:00 +0100

    Versioning the API

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
+require 'api_constraints'
+
 OneStore::Application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
